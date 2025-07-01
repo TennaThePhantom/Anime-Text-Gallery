3775d22e433457a59c3cef80a8aa844e2fbe821f
Tennessee Foster <tennatenna99@outlook.com>
2025-07-01 17:16:02 -0400

    react icons dark and light mode up setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { MainScreenTextData } from "./assets/data/textData.js";
 import MainScreenText from "./assets/components/MainScreenText.jsx";
+import DarkLightMode from "./assets/components/DarkLightMode.jsx";
 function App() {
 	return (
 		<div className="app-component">
+			<DarkLightMode/>
 			<div className="text-container">
 				{/* currently display the text for the anime gallery*/}
 				{MainScreenTextData.map((text, index) => (
---
+++ src/assets/components/DarkLightMode.jsx
@@ -0,0 +1,14 @@
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
+
+
+function DarkLightMode() {
+  return (
+    <div className="darkLightMode">
+        <MdLightMode />
+    </div>
+  );
+}
+
+
+export default DarkLightMode
