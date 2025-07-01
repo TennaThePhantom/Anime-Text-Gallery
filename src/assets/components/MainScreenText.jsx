89dff6763487c0ce4159738c694f206979449213
Tennessee Foster <tennatenna99@outlook.com>
2025-07-01 16:51:47 -0400

    uppercase main screen text

--- src/assets/components/MainScreenText.jsx
+++ src/assets/components/MainScreenText.jsx
@@ -3,7 +3,7 @@ function MainScreenText({text}) {
 	return (
         // This component displays text on the main screen.
         <div className="main-screen-text">
-            <p>{text}</p>
+            <p>{text.toUpperCase()}</p>
         </div>
 	);
 }
