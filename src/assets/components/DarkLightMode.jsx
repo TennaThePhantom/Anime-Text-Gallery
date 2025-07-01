e961e96f42c053e5e43ec5634c6b5af21bcf93ba
Tennessee Foster <tennatenna99@outlook.com>
2025-07-01 18:48:54 -0400

    dark&light mode icon

--- src/assets/components/DarkLightMode.jsx
+++ src/assets/components/DarkLightMode.jsx
@@ -1,14 +1,46 @@
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
-
+import "../CSS/DarkLightMode.css";
+import { useState, useEffect } from "react";
 
 function DarkLightMode() {
-  return (
-    <div className="darkLightMode">
-        <MdLightMode />
-    </div>
-  );
+	// State to manage dark mode basically if darkMode is true then dark mode is enabled otherwise light mode is enabled
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const checkUserPreference = window.matchMedia(
+				"(prefers-color-scheme: dark)"
+			).matches;
+			setIsDarkMode(checkUserPreference);
+			if (checkUserPreference) {
+				document.documentElement.classList.add("dark");
+			} else {
+				document.documentElement.classList.remove("dark");
+			}
+		}
+	}, []);
+
+	const toggleDarkMode = () => {
+        const newMode = !isDarkMode
+		setIsDarkMode(newMode);
+
+		if (newMode) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+	};
+
+	return (
+		<div className="darkLightMode" onClick={toggleDarkMode}>
+			{isDarkMode ? (
+				<MdLightMode className="DLM-icon" /> // shows light mode icon when dark mode is enabled
+			) : (
+				<MdDarkMode className="DLM-icon" /> // shows dark mode icon when light mode is enabled
+			)}
+		</div>
+	);
 }
 
-
-export default DarkLightMode
+export default DarkLightMode;
