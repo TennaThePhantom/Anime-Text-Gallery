660e6d44d1a9674f76f458b858cb45c95fe935eb
Tennessee Foster <tennatenna99@outlook.com>
2025-07-07 19:43:31 -0400

    kuroko basketball css  animation

--- src/assets/components/MainScreenText.jsx
+++ src/assets/components/MainScreenText.jsx
@@ -1,19 +1,45 @@
-import '../CSS/MainScreenText.css';
-function MainScreenText({text, index}) {
-    const textAnimationsClasses = [
-        "dragon-ball-text-animated",
-        "bleach-text-animated",
-        "black-clover-animated-text",
-        "fate-series-animated-text"
+import "../CSS/MainScreenText.css";
+import React, { useEffect } from "react";
+function MainScreenText({ text, index }) {
+	const textAnimationsClasses = [
+		"dragon-ball-text-animated",
+		"bleach-text-animated",
+		"black-clover-animated-text",
+		"fate-series-animated-text",
+		"kuroko-basketball-animated-text",
+	];
 
-    ]
+	useEffect(() => {
+		const kurokoAnimatedText = document.querySelector(
+			".kuroko-basketball-animated-text"
+		);
+		if (kurokoAnimatedText) {
+			const kurokoText = kurokoAnimatedText.textContent;
+			kurokoAnimatedText.innerHTML = "";
 
-    const textAnimation = textAnimationsClasses[index % textAnimationsClasses.length]
+			kurokoText.split("").forEach((char, charIndex) => {
+				const spanText = document.createElement("span");
+				spanText.className = `char-${charIndex + 1}`;
+				spanText.textContent = char;
+
+				if (charIndex === 6 || charIndex === 8) {
+					spanText.style.color = "text-zinc-300";
+				} else {
+				spanText.classList.add('color-char');
+				}
+
+				kurokoAnimatedText.appendChild(spanText);
+			});
+		}
+	}, []);
+
+	const textAnimation =
+		textAnimationsClasses[index % textAnimationsClasses.length];
 	return (
-        // This component displays text on the main screen.
-        <div className={`main-screen-text ${textAnimation}`}>
-            <p>{text.toUpperCase()}</p>
-        </div>
+		// This component displays text on the main screen.
+		<div className={`main-screen-text ${textAnimation}`}>
+			<p>{text.toUpperCase()}</p>
+		</div>
 	);
 }
 export default MainScreenText;
