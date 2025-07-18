4c37d1de57338407c0954f7be74231d43ec9a42b
Tennessee Foster <tennatenna99@outlook.com>
2025-07-18 17:16:33 -0400

    new text data for future testing for clickable text

--- src/assets/data/textData.js
+++ src/assets/data/textData.js
@@ -7,3 +7,38 @@ export const MainScreenTextData = [
 	"Sword Art Online",
 	"Solo Leveling",
 ];
+
+export const SoloLevelingTextData = [
+	"Sung Jin-woo"
+]
+
+export const KurukoBasketballTextData = {
+	"Seirin High": [
+		"Aido Riko",
+		"Hyuga Junpei",
+		"Izuki Shuun",
+		"Koganei Shinji",
+		"Mitobe	Rinnosuke",
+		"Furihata Kouki",
+	],
+	"Generation of Miracles": [
+		"Kuroko Tetsuya",
+		"Aomine Daiki",
+		"Akashi Seijuuro",
+		"Midorima Shintarou",
+		"Murasakibara Atsushi",
+		"Kise Ryouta"
+	],
+	"Uncrowned Kings": [
+		"Teppei Kiyoshi",
+		"Makoto Hanamiya",
+		"Reo Mibuchi",
+		"Kotaro Hayama",
+		"Eikichi Nebuya"
+	],
+	"Americans": [
+		"Taiga Kagami",
+		"Tatsuya Himuro",
+		"Alexandra Garcia",
+	]
+}
