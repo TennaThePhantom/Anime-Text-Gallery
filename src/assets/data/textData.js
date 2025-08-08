1a330f78a1146c10a55f8b9d66f6157eb9f6d893
Tennessee Foster <tennatenna99@outlook.com>
2025-08-08 18:45:38 -0400

    added text data for the rest of text

--- src/assets/data/textData.js
+++ src/assets/data/textData.js
@@ -12,7 +12,7 @@ export const SoloLevelingTextData = ["Sung Jin-Woo"];
 
 export const KurukoBasketballTextData = {
 	"Seirin High": {
-		"Coach": ["Aido Riko"],
+		Coach: ["Aido Riko"],
 		"First Year": ["Furihata Kouki", "Fukuda Hiroshi", "Kawahara Kouichi"],
 		"Second Year": [
 			"Mitobe Rinnosuke",
@@ -37,5 +37,120 @@ export const KurukoBasketballTextData = {
 		"Kotaro Hayama",
 		"Eikichi Nebuya",
 	],
-	"Americans": ["Taiga Kagami", "Tatsuya Himuro", "Alexandra Garcia"],
+	Americans: ["Taiga Kagami", "Tatsuya Himuro", "Alexandra Garcia"],
 };
+
+export const BleachTextData = {
+	"Gotei 13": {
+		"Old Gotei 13": {},
+		"Current Gotei 13": {},
+	},
+	Espapdes: {
+		Leaders: ["Sosuke Aizen", "gin Ichimaru", "Kaname Tosen"],
+		Members: {
+			"Espades 1-5": [],
+			"Espades 6-10": [],
+		},
+	},
+	"The Visored": [
+		"Shinji Hirako",
+		"Love Aikawa",
+		"Rōjūrō Ōtoribashi",
+		"Kensei Muguruma",
+		"Mashiro Kuna",
+		"Hiyori Sarugaki",
+		"Hachigen Ushōda",
+		"Lisa Yadōmaru",
+	],
+	"The Royal Guard": [],
+	"Main Characters": [
+		"Ichigo Kurosaki",
+		"Rukia Kuchiki",
+		"Renji Abarai",
+		"Orihime Inoue",
+		"Uryu Ishida",
+		"Yasutora Sado",
+		"Yoruichi Shihouin",
+		"Kisuke Urahara",
+	],
+};
+
+export const blackCloverTextData = {
+	"Black Bulls": {
+		Leaders: [],
+		Members: {
+			"1st class magic knights": [],
+			"3rd class magic knights": [],
+			"5th class magic knights": [],
+			"Other Members": [],
+		},
+	},
+	"Squad Captains": {
+		"Top 3 Squad Captains": [
+			"William Vangeance",
+			"Yami Sukehiro",
+			"Nozel Silva",
+		],
+		"Other Squad Captains": [
+			"Charlotte Roselei",
+			"Fuegoleon Vermillion",
+			"Jack the Ripper",
+			"Dorothy Unsworth",
+			"Kaiser Granvorka",
+			"Rill Boismortier",
+		],
+		"Sub-Previous Squad Captains": [
+			"Yuno Grinberryall",
+			"Mereonleona Vermillion",
+			"Gueldre Poizot",
+		],
+	},
+	"Dark Triad": [],
+	"Eye of the Midnight Sun": [],
+	"Asta Companions": [],
+	"Wizard Kings": {
+		"Julius Novachrono": "Current Wizard King",
+		"Previous Wizard Kings": [],
+	},
+};
+
+export const fateSeriesTextData = {
+	"Fate/Zero": {
+		Masters: [],
+		Servants: [],
+		"Other Characters": [],
+	},
+	"Fate/Stay Night: Unlimited Blade Works": {
+		Masters: [],
+		Servants: [],
+		"Other Characters": [],
+	},
+	"Fate/Stay Night: Heaven's Feel": {
+		Masters: [],
+		Servants: [],
+		"Other Characters": [],
+	},
+	"Fate/Grand Order Absolute Demonic Front: Babylonia": {
+		Masters: [],
+		Servants: [],
+		"Other Characters": [],
+	},
+};
+
+export const SwordArtOnlineTextData = {
+	"Kirigaya Kazuto": "Main Protagonist",
+	"Yuuki Asuna": "Kirigaya Partner",
+	"Kirigaya Friends": []
+}
+
+export const DragonBallTextData = {
+	"Z Fighters": [],
+	"Villains": [],
+	"Movie Villains": [],
+	"Fusions": [],
+	"Saiyans": [],
+	"Secondary Characters": [],
+
+
+
+}
