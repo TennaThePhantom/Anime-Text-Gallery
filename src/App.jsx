a4f69f6ee2342064e6d3776562332e5fdef9a91b
Tennessee Foster <tennatenna99@outlook.com>
2025-08-22 22:30:22 -0400

    Mobile warning message

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import DarkLightMode from "./assets/components/DarkLightMode.jsx";
 import Volume from "./assets/components/Volume.jsx";
 import NavigationWrapper from "./assets/components/NavigationWrapper.jsx";
 import { AudioProvider } from "./assets/Contexts/AudioContext";
+import MobileAppWaring from "./assets/components/MobileAppWaring.jsx";
 
 // work on volume component tomorrow
 function App() {
 	return (
 		<AudioProvider>
 			<div className="app-component">
+				<MobileAppWaring />
+
 				<DarkLightMode />
 				<Volume />
 				<div className="text-container">
---
+++ src/assets/components/MobileAppWaring.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import useWindowSize from "../Hooks/useWindowSize.jsx";
+
+const MobileAppWaring = () => {
+	const { width: screenWidth } = useWindowSize();
+	const mobileWarning = "Mobile"
+	useEffect(() => {
+		const checkScreenSize = () => {
+			if (screenWidth <= 768) {
+				toast.warning(
+					" Demo won't work as expected on smaller screen sizes",
+					{
+						position: "bottom-center",
+						autoClose: 2500,
+						hideProgressBar: true,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						toastId: mobileWarning,
+					}
+				);
+			}
+		};
+
+		checkScreenSize();
+
+		window.addEventListener("resize", checkScreenSize);
+
+		return () => window.removeEventListener("resize", checkScreenSize);
+	}, []);
+
+	return <ToastContainer />;
+};
+
+export default MobileAppWaring;
